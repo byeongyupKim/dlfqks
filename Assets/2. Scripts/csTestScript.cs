8c17bfe4a957b4cfac696cbe0424b56cd37b02e2
opentutorials_by <kby7139@gmail.com>
2017-06-30 16:33:40 +0900

    <kby> 도대체 뭐가 잘못된 것인지 모르겠다. Project update

---
+++ Assets/2. Scripts/csTestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class csTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Text thisText = this.GetComponent<Text>();
+
+        thisText.text = "I hate Hello World!! \nhahaha!! \nkby";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
